d42c0e485c46875c196556bbe32722b8cf70df2b
mansiasati <mansiasati10112000@gmail.com>
2023-09-13 15:54:48 +0530

    api created

--- HomeWork/src/main/java/com/example/demo/controller/HomeController.java
+++ HomeWork/src/main/java/com/example/demo/controller/HomeController.java
@@ -3,13 +3,14 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Student;
 import com.example.demo.service.HomeService;
-
+import java.util.*;
 @RestController
 public class HomeController 
 {
@@ -20,5 +21,11 @@ public ResponseEntity<Student> m1(@RequestBody Student s)
 {
 	Student s1=hs.m1(s);
 	return new ResponseEntity<Student>(s1, HttpStatus.OK);
+}
+@GetMapping("/get")
+public ResponseEntity<List<Student>> getalldata()
+{
+	List<Student>s=hs.getalldata();
+	return new ResponseEntity<List<Student>>(s, HttpStatus.OK);
 }
 }
--- HomeWork/src/main/java/com/example/demo/service/HomeService.java
+++ HomeWork/src/main/java/com/example/demo/service/HomeService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
+import java.util.*;
 import com.example.demo.model.Student;
 
 public interface HomeService {
 
 	public Student m1(Student s);
+
+	public List<Student> getalldata();
 
 }
